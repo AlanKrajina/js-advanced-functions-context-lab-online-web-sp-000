6e5ef1cb3d62db9cec9bb6528f5aed08093db3bb
Alan Krajina <alankrajina88@gmail.com>
2020-01-04 11:00:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,36 @@ function createEmployeeRecords(twoRows){
   return twoRows.map(person => createEmployeeRecord(person))
 }
 
+function createTimeInEvent(record,date){
+  let newTime = {
+    type: 'TimeIn',
+    hour: parseInt(date.slice(-4),10),
+    date: date.slice(0,10)
+  }
+  record.timeInEvents.push(newTime)
+  return record
+}
 
+function createTimeOutEvent(record,date){
+  let newTime = {
+    type: 'TimeOut',
+    hour: parseInt(date.slice(-4),10),
+    date: date.slice(0,10)
+  }
+  record.timeOutEvents.push(newTime)
+  return record
+}
+
+function hoursWorkedOnDate(record, date) {
+    let timeIn = record.timeInEvents.find(f => f.date === date)
+    let timeOut = record.timeOutEvents.find(f => f.date === date)
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(record, date) {
+    let hours = hoursWorkedOnDate(record,date)
+    return hours * record.payPerHour
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
